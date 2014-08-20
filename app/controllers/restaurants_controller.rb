e9a8dfed2c3ebaec3b75a1e438ae7376ac50cdf0
Wax <jwaxnyc@gmail.com>
2014-08-20 16:34:16 -0400

    create restaurant migration, update routes file, update restaurant model

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,5 @@
+class RestaurantsController < ApplicationController
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  
+  resources :restaurants, only: [:new, :create, :show, :index] 
 end

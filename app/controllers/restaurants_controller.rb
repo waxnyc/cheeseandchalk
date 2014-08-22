90fcec553c43b12fe9cb68e8d239a3329a637ca2
Wax <jwaxnyc@gmail.com>
2014-08-21 20:20:10 -0400

    simple changes to two controllers and sesions new page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,16 @@
 class RestaurantsController < ApplicationController
-  def new
-    @restaurant = Restaurant.new
+  def index
+    @restaurants = Restaurant.all 
   end
 
-  def index
-    @restaurant = Restaurants.all 
+  def new
+    @restaurant = Restaurant.new
   end
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
-    redirect_to restaurant_path
+    redirect_to restaurants_path
   end
 
   def show
